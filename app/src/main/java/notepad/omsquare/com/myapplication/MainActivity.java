422195437d3513b0cc7482e887aba90aa28e1eaa
moseqi <moseqi7990@gmail.com>
2018-08-11 18:53:10 +0530

    Added change text value method

--- app/src/main/java/notepad/omsquare/com/myapplication/MainActivity.java
+++ app/src/main/java/notepad/omsquare/com/myapplication/MainActivity.java
@@ -1,13 +1,31 @@
 package notepad.omsquare.com.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvHelloWorld;
+    Button btClickMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvHelloWorld = findViewById(R.id.tvHelloWorld);
+        btClickMe = findViewById(R.id.btClickMe);
+        btClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeTestValue();
+            }
+        });
     }
+
+    private void changeTestValue() {
+        tvHelloWorld.setText("Hello Android");
+    }
+
 }
